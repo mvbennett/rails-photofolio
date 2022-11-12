7825e59820abf594c782d9d5162496e5fc5410f1
Mike Bennett <mvbennett9@gmail.com>
2022-11-11 21:18:58 -0500

    created routes for a local photo api

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # API
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :photos, only: [:index]
+    end
+  end
 end
