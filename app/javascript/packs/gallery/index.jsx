c4aac3adcb7dc0d996260d4147fd7b2b123d02fc
Michael Bennett <mvbennett9@gmail.com>
2022-11-25 21:05:23 -0500

    deleted commented out import

--- app/javascript/packs/gallery/index.jsx
+++ app/javascript/packs/gallery/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import '../assets/stylesheets/application.scss';
-
 import App from './components/App';
 
 const container = document.getElementById('root');
