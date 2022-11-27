b71524e84b84758532b1933ed3e398c28f8c31de
Michael Bennett <mvbennett9@gmail.com>
2022-11-26 20:19:35 -0500

    photos will be seeded correctly now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,18 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+  # def home
+  #   @user = User.first
+  #   @photos = []
+  #   @user.photos.each { |photo| @photos.push([photo.key, photo.url]) }
+  #   @photo_hash = @photos.to_h
+  # end
+
   def home
-    @user = User.first
-    @photos = []
-    @user.photos.each { |photo| @photos.push([photo.key, photo.url]) }
-    @photo_hash = @photos.to_h
+    user = User.first
+    photos = []
+    user.galleries.first.photographs.each { |photograph| photos.push([photograph.photo.key, photograph.photo.url]) }
+    @photo_hash = photos.to_h
   end
 
   def edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 
+Photograph.destroy_all
+Gallery.destroy_all
 User.destroy_all
 
 mike = User.new(email: 'mike@mike.com', password: ENV['password'])
@@ -16,23 +18,49 @@ gallery = Gallery.new(title: 'all')
 gallery.user = mike
 gallery.save!
 
-photo_one = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/1_elpxo7.jpg')
-photo_two = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/2_rcridv.jpg')
-photo_three = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/7_tuukaw.jpg')
-photo_four = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/6_blq1xj.jpg')
-photo_five = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/8_lefayh.jpg')
-photo_six = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/9_nhfgxe.jpg')
-photo_seven = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/3_iirf83.jpg')
-photo_eight = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/4_ldcjwt.jpg')
-photo_nine = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/5_kzoraz.jpg')
+uploads = [
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/1_elpxo7.jpg',
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/2_rcridv.jpg',
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/7_tuukaw.jpg',
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/6_blq1xj.jpg',
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/8_lefayh.jpg',
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/9_nhfgxe.jpg',
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/3_iirf83.jpg',
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/4_ldcjwt.jpg',
+  'https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/5_kzoraz.jpg'
+]
 
-mike.photos.attach(io: photo_one, filename: '1.jpg', content_type: 'image/jpg')
-mike.photos.attach(io: photo_two, filename: '1.jpg', content_type: 'image/jpg')
-mike.photos.attach(io: photo_three, filename: '1.jpg', content_type: 'image/jpg')
-mike.photos.attach(io: photo_four, filename: '1.jpg', content_type: 'image/jpg')
-mike.photos.attach(io: photo_five, filename: '1.jpg', content_type: 'image/jpg')
-mike.photos.attach(io: photo_six, filename: '1.jpg', content_type: 'image/jpg')
-mike.photos.attach(io: photo_seven, filename: '1.jpg', content_type: 'image/jpg')
-mike.photos.attach(io: photo_eight, filename: '1.jpg', content_type: 'image/jpg')
-mike.photos.attach(io: photo_nine, filename: '1.jpg', content_type: 'image/jpg')
+uploads.each_with_index do |url, index|
+  photo = URI.open(url)
+  photograph = Photograph.new(title: "#{index + 1}")
+  photograph.gallery = gallery
+  photograph.photo.attach(io: photo, filename: "#{index + 1}.jpg", content_type: 'image/jpg')
+  photograph.save!
+end
+
 mike.save!
+
+mike.galleries.first.photographs.each do |pho|
+  puts pho.title
+end
+
+# photo_one = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/1_elpxo7.jpg')
+# photo_two = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/2_rcridv.jpg')
+# photo_three = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295281/photofolio/7_tuukaw.jpg')
+# photo_four = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/6_blq1xj.jpg')
+# photo_five = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/8_lefayh.jpg')
+# photo_six = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295279/photofolio/9_nhfgxe.jpg')
+# photo_seven = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/3_iirf83.jpg')
+# photo_eight = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/4_ldcjwt.jpg')
+# photo_nine = URI.open('https://res.cloudinary.com/dduinomn4/image/upload/v1668295277/photofolio/5_kzoraz.jpg')
+
+# mike.photos.attach(io: photo_one, filename: '1.jpg', content_type: 'image/jpg')
+# mike.photos.attach(io: photo_two, filename: '1.jpg', content_type: 'image/jpg')
+# mike.photos.attach(io: photo_three, filename: '1.jpg', content_type: 'image/jpg')
+# mike.photos.attach(io: photo_four, filename: '1.jpg', content_type: 'image/jpg')
+# mike.photos.attach(io: photo_five, filename: '1.jpg', content_type: 'image/jpg')
+# mike.photos.attach(io: photo_six, filename: '1.jpg', content_type: 'image/jpg')
+# mike.photos.attach(io: photo_seven, filename: '1.jpg', content_type: 'image/jpg')
+# mike.photos.attach(io: photo_eight, filename: '1.jpg', content_type: 'image/jpg')
+# mike.photos.attach(io: photo_nine, filename: '1.jpg', content_type: 'image/jpg')
+# mike.save!
